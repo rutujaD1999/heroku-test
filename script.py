c5af391fad4cd5391ab1de377ead5652e3aa2158
rutujaD1999 <42310451+rutujaD1999@users.noreply.github.com>
2020-12-17 19:58:15 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,16 @@
-print testing
+import datetime
+import numpy as np
+from matplotlib import cm, pyplot as plt
+import pandas as pd
+from pandas_datareader import data
+import seaborn as sns
+from sklearn import tree
+import talib
+
+duration = 7
+df = data.DataReader("GAIL.NS", start='2014-1-1', end='2020-12-06', data_source='yahoo')
+df.to_csv("SBIN.csv")
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+df=pd.read_csv("SBIN.csv")
+df.tail()
